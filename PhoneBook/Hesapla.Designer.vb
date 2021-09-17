41c7cf5907f300e7395c3bc28f3ee73e1d48a5c5
onur topaloğlu <topaloglu08@gmail.com>
2021-09-17 14:52:45 +0300

    hesapla üzerinde değişiklikler

--- PhoneBook/Hesapla.Designer.vb
+++ PhoneBook/Hesapla.Designer.vb
@@ -25,11 +25,14 @@ Partial Class Hesapla
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.ComboBox2 = New System.Windows.Forms.ComboBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"2021", "2022", "2023"})
         Me.ComboBox1.Location = New System.Drawing.Point(84, 39)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 23)
@@ -45,12 +48,30 @@ Partial Class Hesapla
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(472, 140)
+        Me.Button1.Location = New System.Drawing.Point(130, 134)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Hesapla"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Yıl"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Ay"
         '
         'Hesapla
         '
@@ -58,7 +79,9 @@ Partial Class Hesapla
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoScroll = True
         Me.AutoSize = True
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(390, 312)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ComboBox2)
         Me.Controls.Add(Me.ComboBox1)
@@ -67,10 +90,13 @@ Partial Class Hesapla
         Me.Text = "Hesapla"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents ComboBox2 As ComboBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
