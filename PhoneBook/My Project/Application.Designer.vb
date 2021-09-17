c95c4f44b898a7041ef0a9b26cbd3a15569c42a8
onur topaloğlu <topaloglu08@gmail.com>
2021-09-17 16:04:33 +0300

    değişiklikler

--- PhoneBook/My Project/Application.Designer.vb
+++ PhoneBook/My Project/Application.Designer.vb
@@ -34,10 +34,5 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.ApartmentManagement.MainForm
         End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Sub OnCreateSplashScreen()
-            Me.SplashScreen = Global.ApartmentManagement.Form1
-        End Sub
     End Class
 End Namespace
